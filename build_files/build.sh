b5d6b44aef119e226bb5af13d99e84af11e99565
ajrpayne <ajrpayne@gmail.com>
2025-11-08 23:13:40 -0800

    Add multiple package installations to build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,65 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux
+
+# https://pagure.io/workstation-ostree-config/blob/f43/f/packages/sway-atomic.yaml
+dnf5 install -y \
+  NetworkManager-l2tp-gnome \
+  NetworkManager-libreswan-gnome \
+  NetworkManager-openconnect-gnome \
+  NetworkManager-openvpn-gnome \
+  NetworkManager-sstp-gnome \
+  NetworkManager-vpnc-gnome \
+  Thunar \
+  blueman \
+  bolt \
+  dunst \
+  foot \
+  fprintd-pam \
+  gnome-keyring-pam \
+  gnome-themes-extra \
+  grim \
+  gvfs \
+  gvfs-smb \
+  imv \
+  kanshi \
+  lxqt-policykit \
+  network-manager-applet \
+  pavucontrol \
+  pinentry-gnome3 \
+  playerctl \
+  plymouth-system-theme \
+  polkit \
+  pulseaudio-utils \
+  sddm \
+  sddm-wayland-sway \
+  slurp \
+  sway \
+  sway-config-fedora \
+  swaybg \
+  swayidle \
+  swaylock \
+  system-config-printer \
+  thunar-archive-plugin \
+  tuned-ppd \
+  tuned-switcher \
+  waybar \
+  wev \
+  wl-clipboard \
+  wlr-randr \
+  wlsunset \
+  xarchiver \
+  xdg-desktop-portal-gtk \
+  xdg-desktop-portal-wlr \
+  xorg-x11-server-Xwayland
+
+# https://github.com/ublue-os/main/blob/9a4fca91cf190dbfeba2ff0628cf75efdff8f31c/packages.json
+dnf5 install -y \
+  clipman \
+  gvfs-mtp \
+  thunar-volman \
+  tumbler
 
 # Use a COPR Example:
 #
